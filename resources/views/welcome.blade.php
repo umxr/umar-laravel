d5afa05726def247dfd85553f992cbc8612f374f
Umar <umarg1997@gmail.com>
2023-11-15 22:34:37 +0000

    init commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="text-black bg-white dark:text-white dark:bg-[#111010]">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Umar Gora</title>
+        <meta content="Founder, engineer, and traveller." name="description">
+        <meta content="index, follow" name="robots">
+        <meta
+            content="index, follow, max-video-preview:-1, max-image-preview:large, max-snippet:-1"
+            name="googlebot"
+        >
+        <meta content="Umar Gora" property="og:title">
+        <meta
+            content="Founder, engineer, and traveller."
+            property="og:description"
+        >
+        <meta content="https://umar.codes/" property="og:url">
+        <meta content="Umar Gora" property="og:site_name">
+        <meta content="en_GB" property="og:locale">
+        <meta content="website" property="og:type">
+        <meta content="summary_large_image" name="twitter:card">
+        <meta content="Umar Gora" name="twitter:title">
+        <meta
+            content="Founder, engineer, and traveller."
+            name="twitter:description"
+        >
+        @vite('resources/css/app.css')
+    </head>
+    <body class="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
+        <main class="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+            Hello World
+        </main>
+    </body>
+</html>
